399c13889ff770bb7a384bdf380dac48cf3efe38
zapodask <joaopedroemerim@gmail.com>
2023-11-15 23:11:06 -0300

    feat: add CompanyAccount getLoan method

--- class/CompanyAccount.ts
+++ class/CompanyAccount.ts
@@ -1,12 +1,17 @@
-import { DioAccount } from "./DioAccount"
+import { DioAccount } from './DioAccount'
 
 export class CompanyAccount extends DioAccount {
-
   constructor(name: string, accountNumber: number){
     super(name, accountNumber)
   }
 
-  getLoan = (): void => {
-    console.log('Voce pegou um empréstimo')
+  getLoan = (value: number): void => {
+    if (!this.validateStatus()) {
+      throw new Error('Conta inválida')
+    }
+
+    this.addBalance(value)
+
+    console.log(`Voce solicitou um emprestimo de R$${value} e seu saldo atual é R$${this.getBalance()}`)
   }
 }
