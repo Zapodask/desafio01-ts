5552ac36bf8b9c807876946f808144ac3053980f
zapodask <joaopedroemerim@gmail.com>
2023-11-15 23:11:37 -0300

    feat: code demonstration

--- app.ts
+++ app.ts
@@ -1,9 +1,18 @@
 import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
+import { VipAccount } from './class/VipAccount'
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
+peopleAccount.deposit(25)
+peopleAccount.withdraw(5)
+console.log(peopleAccount.getName())
+console.log(peopleAccount.getAccountNumber())
+console.log(peopleAccount.getBalance())
+
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+companyAccount.getLoan(250)
+console.log(companyAccount.getBalance())
+
+const vipAccount = new VipAccount('Yan', 30)
+vipAccount.deposit(50)
+console.log(vipAccount.getBalance())
